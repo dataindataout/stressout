421ef0dbc094153dec0cd5b184bebb65780d2cea
Valerie Parham-Thompson <valerie.parham.thompson@gmail.com>
2016-05-29 00:40:55 -0400

    database access, time parsing, file writes

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # stressout
 Stressing out your database.
 
-# Enable your general log
-Run the setup script here: setup coming.
+# Install dependencies
+
+You need the mysql client if you're running this on a remote machine. Here's one way to get that:
+> brew install mariadb
+
+Install lua (luarocks will come with it) and the luasql-mysql and date modules:
+> brew install lua
+> luarocks install luasql-mysql MYSQL_DIR=/usr/local/bin MYSQL_INCDIR=/usr/local/Cellar/mariadb/10.1.14/include/mysql
+> luarocks install date
+
+# Enable the general log in MySQL
+
+Turn on your general log and store results in a table.
+
+> mysql -uuser -ppassword -e"set global log_output='TABLE'"
+> mysql -uuser -ppassword -e"set global general_log='ON'"
+
+Don't forget to periodically truncate the general log table and consider turning it off after gathering queries, to avoid filling up your disk, especially on a busy server.
+
+# Convert the general log table to MyISAM for indexed searches.
+
+> mysql -uuser -ppassword -e"set global general_log='OFF'"
+> mysql -uuser -ppassword -e"alter table mysql.general_log engine=MyISAM"
+> mysql -uuser -ppassword -e"alter table mysql.general_log add index(event_time)"
+> mysql -uuser -ppassword -e"alter table mysql.general_log add index(thread_id)"
+> mysql -uuser -ppassword -e"set global general_log='ON'"
+
+# After allowing time for the queries to be logged, parse the log into per-thread, timed query playbooks.
+
+> ./convert.lua
+
---
+++ convert.lua
@@ -0,0 +1,108 @@
+#!/usr/local/bin/lua
+
+-- Script to convert MySQL general log
+-- into a replayable, timed list of queries
+
+-- This is also my lua hello world :)
+
+-- Valerie Parham-Thompson for sharing 2016
+
+-- TODO: error handling
+-- TODO: cross-platform testing
+
+-- #################################################################################
+
+-- Open a MySQL connection using parameters passed into script
+
+mysql = require "luasql.mysql"
+date = require "date"
+
+local env  = mysql.mysql()
+local conn = env:connect('mysql','valerie','password','192.168.1.234')
+
+-- Capture current starting and ending position
+
+cursor_capture_times,errorString = conn:execute("select min(event_time) as capture_start_time, max(event_time) as capture_end_time from mysql.general_log")
+
+row_capture_times = cursor_capture_times:fetch ({}, "start")
+
+while row_capture_times do
+   capture_start_time = row_capture_times.capture_start_time
+   capture_end_time = row_capture_times.capture_end_time
+   row_capture_times = cursor_capture_times:fetch (row_capture_times, "start")
+end
+
+cursor_capture_times:close()
+
+-- Loop through thread IDs within the starting and ending position
+
+cursor_threads,errorString = conn:execute("select thread_id, event_time from mysql.general_log where command_type='Connect' and event_time>'" .. capture_start_time .. "'and event_time<'" .. capture_end_time .. "'")
+
+row_threads = cursor_threads:fetch ({}, "threads")
+
+while row_threads do
+
+   local current_thread_id = row_threads.thread_id
+   local current_thread_start_time = row_threads.event_time
+
+    -- open the main scheduler file
+    scheduler_file = io.open("scheduler.lua", "a")
+   io.output(scheduler_file)
+
+   io.write("-- ================\n")
+   io.write(current_thread_id .. ".lua\n")
+   thread_start_offset = date.diff(current_thread_start_time,capture_start_time):spanseconds()
+   io.write(thread_start_offset .. "\n")
+   io.write("-- " .. current_thread_start_time .. "\n")
+   
+   io.close(scheduler_file)
+
+   -- open individual thread files
+   thread_file = io.open(current_thread_id .. ".lua", "a")
+   io.output(thread_file)
+
+   io.write("-- ================\n")
+   io.write("-- Thread ID: " .. current_thread_id .. "\n")
+   io.write("-- Thread Start Time: " .. current_thread_start_time .. "\n")
+   io.write("-- ================\n")
+
+       cursor_thread_details,errorString = conn:execute("select event_time, argument from mysql.general_log where command_type='Query' and thread_id='" .. current_thread_id .. "'")
+
+       row_thread_details = cursor_thread_details:fetch ({}, "thread_details")
+
+       -- initialize
+       last_thread_detail_start_time = current_thread_start_time
+
+       while row_thread_details do
+
+          thread_detail_start_time = row_thread_details.event_time
+          io.write("-- Query Start Time: " .. thread_detail_start_time .. "\n")
+          -- io.write("-- Last Start Time : " .. last_thread_detail_start_time .. "\n")
+
+          local thread_detail_start_offset = date.diff(thread_detail_start_time,last_thread_detail_start_time):spanseconds()
+          local thread_detail_query = row_thread_details.argument
+          
+          -- this will be a function calling os.execute("sleep " .. tonumber(n))
+          io.write("sleep(" .. thread_detail_start_offset .. ")\n")
+          io.write("rs = db_query('" .. thread_detail_query .. "')\n")
+
+          io.write("-- ================\n")
+
+          last_thread_detail_start_time = thread_detail_start_time
+
+          row_thread_details = cursor_thread_details:fetch (row_thread_details, "thread_details")
+       end
+
+       cursor_thread_details:close()
+
+   row_threads = cursor_threads:fetch (row_threads, "threads")
+
+   io.close(thread_file)
+
+end
+
+-- close all
+cursor_threads:close()
+
+conn:close()
+env:close()
