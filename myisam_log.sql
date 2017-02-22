201bd49cd511d41eb89df131519437dc2949053b
Valerie Parham-Thompson <valerie@opengroupware.ch>
2017-02-21 21:56:35 -0500

    sql file for converting general log to myisam

--- myisam_log.sql
+++ myisam_log.sql
@@ -1,3 +1,8 @@
+# turn on general logging, saved to a table
+set global log_output='TABLE';
+set global general_log='ON';
+
+# convert log to myisam for quicker access by later scripts
 set global general_log='OFF';
 alter table mysql.general_log engine=MyISAM;
 alter table mysql.general_log add index(event_time);
